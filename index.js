5de95d628894301f9cc557dac74fd3aad17bb241
Arthur-codeX <daniel.karanja239@gmail.com>
2022-02-14 02:26:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = (name) => cats.push(name);
+
+const destructivelyPrependCat = (name) => cats.unshift(name);
+
+const destructivelyRemoveLastCat = (name) => cats.pop();
+
+const destructivelyRemoveFirstCat = (name) => cats.shift();
+
+const appendCat = (name) => [...cats, name];
+
+const prependCat = (name) => [name, ...cats];
+
+const removeLastCat = () =>
+  cats.filter((cat, index) => (index == cats.length - 1 ? false : true));
+const removeFirstCat = () =>
+  cats.filter((cat, index) => (index != 0 ? true : false));
+
+console.log(removeLastCat());
